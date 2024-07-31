df86824ca7a5accebfb49368ed4de51a1e606b24
goddesseyes <goddesseyes@protonmail.com>
2024-07-31 22:54:31 +0300

    Use template from doc
    * https://ratatui.rs/templates/

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,36 @@
-use std::io::{self, stdout};
+use yad_tui::app::{App, AppResult};
+use yad_tui::event::{Event, EventHandler};
+use yad_tui::handler::handle_key_events;
+use yad_tui::tui::Tui;
+use std::io;
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
 
-use ratatui::{
-    crossterm::{
-        event::{self, Event, KeyCode},
-        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-        ExecutableCommand,
-    },
-    prelude::*,
-    widgets::*,
-};
+fn main() -> AppResult<()> {
+    // Create an application.
+    let mut app = App::new();
 
-fn main() -> io::Result<()> {
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
 
-    let mut should_quit = false;
-
-    while !should_quit {
-        terminal.draw(ui)?;
-        should_quit = handle_events()?;
-    }
-
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
-    Ok(())
-}
-
-fn handle_events() -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true);
-            }
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next()? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
         }
     }
-    Ok(false)
-}
 
-fn ui(frame: &mut Frame) {
-    let vertical_layouts = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(vec![
-            Constraint::Percentage(20),
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ])
-        .split(frame.size())
-        .to_vec();
-    let previous_dir_block = Block::default()
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded);
-    let current_dir_block = Block::default()
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded);
-    let next_dir_block = Block::default()
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded);
-    let previous_dir = List::new(["root dir"]).block(previous_dir_block);
-    let current_dir = List::new(["x", "y"]).block(current_dir_block);
-    let next_dir = List::new(["z", "z"]).block(next_dir_block);
-    frame.render_widget(previous_dir, vertical_layouts[0]);
-
-    frame.render_widget(current_dir, vertical_layouts[1]);
-    frame.render_widget(next_dir, vertical_layouts[2]);
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
 }
