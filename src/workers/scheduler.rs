800e2c91341d1a2bf4923f2e56fbc9c37ac1797c
honey <lebedev.alexey@protonmail.com>
2025-12-10 23:59:54 +1000

    Impl scheduler.

--- src/workers/scheduler.rs
+++ src/workers/scheduler.rs
@@ -1,37 +1,38 @@
-use std::time::UNIX_EPOCH;
+pub(crate) use std::time::UNIX_EPOCH;
 use std::time::SystemTime;
 use crate::disk_client::DiskClientT;
 use crate::models::model::Model;
 use crate::workers::worker::Worker;
 
-pub struct Scheduler<T: Worker> {
+pub struct Scheduler<'a, T: Worker> {
 
-    pub workers: Vec<T>
+    pub workers: &'a Vec<T>
 }
 
-impl <T: Worker> Scheduler<T> {
+impl <'a, T: Worker> Scheduler<'a, T> {
     
-    pub fn run<D>(self, model: &mut Model<D>) where D: DiskClientT,
+    pub fn run<D>(&self, model: &mut Model<D>) -> Vec<T> where D: DiskClientT,
     {
-        self
+        let updated_workers: Vec<T> = self
             .workers
             .into_iter()
             .map(|w| {
-                let max_delta = 10;
 
                 let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
                 let interval = w.interval().as_secs();
 
-
                 let delta = current_time - w.previous_run_time();
 
                 if delta > interval {
-                    w.run(model)
+                   w.run(model)
                 } else {
-                    w
+                    w.clone()
                 }
-           });
-//            .collect::<T>();
+            })
+            .collect();
+        
+        updated_workers
+        
     }
 
 }
--- src/workers/worker.rs
+++ src/workers/worker.rs
@@ -1,13 +1,22 @@
 use std::sync::mpsc::Sender;
 use std::time::Duration;
 use crate::{disk_client::DiskClientT, models::model::Model};
-
-pub trait Worker {
-
+// Worker represent any background periodic task runnable by Scheduler
+pub trait Worker: Clone {
+    // Value that should be asynced passed into channel Sender, ex:
+    // type SenderValueType = (DirItem, DirItems);
+    //
     type SenderValueType;
-
+    // Min interval between runs in seconds:
+    // 
+    //fn interval(&self) -> Duration {
+    //  Duration::new(60, 0)
+    //}
     fn interval(&self) -> Duration;
+    // Shoud return value to calculate time delta between runs
     fn previous_run_time(&self) -> u64;
+    // Run worker, should be non blocking
     fn run<T>(&self, model: &mut Model<T>) -> Self where T: DiskClientT;
     fn handle(self) -> Self;
 }
+
